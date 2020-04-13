f9e76f63567d62cf138549de2c058a849edf3f94
markevich <slava.markevich@gmail.com>
2020-04-13 14:32:26 +0300

    Closes #54: Rename travel category

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MarkevichMoney.MixProject do
   def project do
     [
       app: :markevich_money,
-      version: "0.3.4",
+      version: "0.3.5",
       elixir: "~> 1.10.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
