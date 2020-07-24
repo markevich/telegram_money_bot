39f3bde959597b313832203c045cf609f68322c5
markevich <slava.markevich@gmail.com>
2020-07-24 10:49:15 +0300

    relates #18: Update packages

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,5 +18,6 @@ window.addEventListener("phx:page-loading-stop", info => NProgress.done())
 liveSocket.connect()
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)
+// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
+// >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
