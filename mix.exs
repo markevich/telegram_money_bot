3cdd99a06eca18363cb0feb8597dc7d35ff0fccc
markevich <slava.markevich@gmail.com>
2020-07-24 10:49:15 +0300

    relates #18: Update packages

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MarkevichMoney.MixProject do
   def project do
     [
       app: :markevich_money,
-      version: "0.3.9",
+      version: "0.3.10",
       elixir: "~> 1.10.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
@@ -54,7 +54,7 @@ defmodule MarkevichMoney.MixProject do
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.4"},
+      {:phoenix_live_dashboard, "~> 0.2"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
