89f6ea24c26e0383384d4573d7418db901a5b414
markevich <slava.markevich@gmail.com>
2020-09-18 18:05:01 +0300

    close #185: Add / commands autocomplete

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MarkevichMoney.MixProject do
   def project do
     [
       app: :markevich_money,
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: "~> 1.10.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
