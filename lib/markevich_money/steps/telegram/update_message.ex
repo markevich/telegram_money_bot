2611b34396fad6d7a33efc624f5ebdf7814396ee
markevich <slava.markevich@gmail.com>
2020-09-26 13:09:25 +0300

    closes #194: Ignore attemp to update message with the same body

--- lib/markevich_money/steps/telegram/update_message.ex
+++ lib/markevich_money/steps/telegram/update_message.ex
@@ -7,23 +7,39 @@ defmodule MarkevichMoney.Steps.Telegram.UpdateMessage do
           chat_id: chat_id
         } = payload
       ) do
-    {:ok, _} =
-      Nadia.edit_message_text(
-        chat_id,
-        message_id,
-        "",
-        output_message,
-        reply_markup: reply_markup,
-        parse_mode: "Markdown"
-      )
+    case Nadia.edit_message_text(chat_id, message_id, "", output_message,
+           reply_markup: reply_markup,
+           parse_mode: "Markdown"
+         ) do
+      {:ok, _result} ->
+        payload
 
-    payload
+      {:error,
+       %Nadia.Model.Error{
+         reason:
+           "Bad Request: message is not modified: specified new message content and reply markup are exactly the same as a current content and reply markup of the message"
+       }} ->
+        payload
+
+      {:error, other_reason} ->
+        raise RuntimeError, message: other_reason
+    end
   end
 
   def call(%{message_id: message_id, output_message: output_message, chat_id: chat_id} = payload) do
-    {:ok, _} =
-      Nadia.edit_message_text(chat_id, message_id, "", output_message, parse_mode: "Markdown")
+    case Nadia.edit_message_text(chat_id, message_id, "", output_message, parse_mode: "Markdown") do
+      {:ok, _result} ->
+        payload
 
-    payload
+      {:error,
+       %Nadia.Model.Error{
+         reason:
+           "Bad Request: message is not modified: specified new message content and reply markup are exactly the same as a current content and reply markup of the message"
+       }} ->
+        payload
+
+      {:error, other_reason} ->
+        raise RuntimeError, message: other_reason
+    end
   end
 end
--- lib/markevich_money_web/controllers/bot_controller.ex
+++ lib/markevich_money_web/controllers/bot_controller.ex
@@ -20,14 +20,26 @@ defmodule MarkevichMoneyWeb.BotController do
       }) do
     callback_data = Jason.decode!(callback_data)
 
-    %CallbackData{
-      chat_id: chat_id,
-      callback_data: callback_data,
-      callback_id: callback_id,
-      message_id: message_id,
-      message_text: message_text
-    }
-    |> Pipelines.call()
+    try do
+      %CallbackData{
+        chat_id: chat_id,
+        callback_data: callback_data,
+        callback_id: callback_id,
+        message_id: message_id,
+        message_text: message_text
+      }
+      |> Pipelines.call()
+    rescue
+      e ->
+        LoggerWithSentry.log_exception(e, __STACKTRACE__, callback_data: callback_data)
+
+        message = "Случилось что то страшное и я не смог обработать запрос."
+
+        SendMessage.call(%{
+          output_message: message,
+          chat_id: chat_id
+        })
+    end
 
     json(conn, %{})
   end
@@ -41,7 +53,7 @@ defmodule MarkevichMoneyWeb.BotController do
       |> Pipelines.call()
     rescue
       e ->
-        LoggerWithSentry.log_exception(e, __STACKTRACE__)
+        LoggerWithSentry.log_exception(e, __STACKTRACE__, input_message: input_message)
 
         message = "Случилось что то страшное и я не смог обработать запрос."
 
