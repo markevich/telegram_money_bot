dbc806d7117673a7cb890d3fc4baf4f9332eb169
markevich <slava.markevich@gmail.com>
2020-11-02 21:14:20 +0300

    Switch host to markevich.money

--- config/prod.exs
+++ config/prod.exs
@@ -9,7 +9,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :markevich_money, MarkevichMoneyWeb.Endpoint,
-  url: [host: "co2offset.dev", port: 80],
+  url: [host: "markevich.money", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
