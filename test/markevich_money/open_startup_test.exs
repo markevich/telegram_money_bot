11abf5c2245d14c6439296a11beeef5d233beed3
markevich <slava.markevich@gmail.com>
2021-03-15 12:54:49 +0300

    [QOL-15] Add credit category.

--- test/markevich_money/open_startup_test.exs
+++ test/markevich_money/open_startup_test.exs
@@ -30,9 +30,9 @@ defmodule MarkevichMoney.OpenStartupTest do
     end
 
     test "list_profits_grouped_by_month/0 returns grouped profits" do
-      insert(:profit, %{amount: 10, date: ~D[2020-10-22]})
-      insert(:profit, %{amount: 10, date: ~D[2020-10-12]})
-      insert(:profit, %{amount: -10, date: ~D[2020-09-12]})
+      insert(:profit, %{amount: 10, date: ~D[2021-02-22]})
+      insert(:profit, %{amount: 10, date: ~D[2021-02-12]})
+      insert(:profit, %{amount: -10, date: ~D[2021-01-12]})
 
       [group1, group2] = OpenStartup.list_profits_grouped_by_month()
 
