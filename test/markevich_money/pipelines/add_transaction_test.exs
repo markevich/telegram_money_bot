5046425df7ae4e299652148776735a1f29a6af0d
markevich <slava.markevich@gmail.com>
2021-03-17 14:49:22 +0300

    [QOL-17, QOL-18] Fix exponential number in stats; Add "," as valid float number character in /add command

--- test/markevich_money/pipelines/add_transaction_test.exs
+++ test/markevich_money/pipelines/add_transaction_test.exs
@@ -12,7 +12,6 @@ defmodule MarkevichMoney.Pipelines.AddTransactionTest do
     setup do
       user = insert(:user)
       to = "Something great"
-      amount = 50.23
       category = insert(:transaction_category)
 
       # prediction
@@ -23,14 +22,16 @@ defmodule MarkevichMoney.Pipelines.AddTransactionTest do
         transaction_category_id: category.id
       )
 
-      %{user: user, category: category, to: to, amount: amount}
+      %{user: user, category: category, to: to}
     end
 
     mocked_test "insert and renders transaction, fire event", context do
       #  FYI: /add 50.23 Something great
+      amount = 50.23
+
       reply_payload =
         Pipelines.call(%MessageData{
-          message: "#{@add_message} #{context.amount} #{context.to}",
+          message: "#{@add_message} #{amount} #{context.to}",
           chat_id: context.user.telegram_chat_id
         })
 
@@ -38,7 +39,41 @@ defmodule MarkevichMoney.Pipelines.AddTransactionTest do
 
       assert(%Transaction{} = transaction)
       assert(transaction.user_id == context.user.id)
-      assert(transaction.amount == Decimal.from_float(-context.amount))
+      assert(transaction.amount == Decimal.from_float(-amount))
+      assert(transaction.transaction_category_id == context.category.id)
+      assert(transaction.to == context.to)
+      assert(transaction.balance == Decimal.new(0))
+
+      assert(Map.has_key?(reply_payload, :output_message))
+      assert(Map.has_key?(reply_payload, :reply_markup))
+
+      assert_called(Nadia.send_message(context.user.telegram_chat_id, _, _))
+
+      assert_enqueued(
+        worker: MarkevichMoney.Gamification.Events.Broadcaster,
+        args: %{
+          event: @transaction_created_event,
+          transaction_id: transaction.id,
+          user_id: context.user.id
+        }
+      )
+    end
+
+    mocked_test "Works correctly when amount with comma", context do
+      #  FYI: /add 50,23 Something great
+      amount = "50,23"
+
+      reply_payload =
+        Pipelines.call(%MessageData{
+          message: "#{@add_message} #{amount} #{context.to}",
+          chat_id: context.user.telegram_chat_id
+        })
+
+      transaction = reply_payload[:transaction]
+
+      assert(%Transaction{} = transaction)
+      assert(transaction.user_id == context.user.id)
+      assert(transaction.amount == Decimal.from_float(-50.23))
       assert(transaction.transaction_category_id == context.category.id)
       assert(transaction.to == context.to)
       assert(transaction.balance == Decimal.new(0))
