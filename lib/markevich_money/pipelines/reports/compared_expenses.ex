f9e927279da27ce7ba7fc9eece588c19c729a2b9
markevich <slava.markevich@gmail.com>
2021-07-20 17:35:03 +0300

    Make it better

--- lib/markevich_money/pipelines/reports/compared_expenses.ex
+++ lib/markevich_money/pipelines/reports/compared_expenses.ex
@@ -2,8 +2,30 @@ defmodule MarkevichMoney.Pipelines.Reports.ComparedExpenses do
   alias MarkevichMoney.Steps.Telegram.SendMessage
   alias MarkevichMoney.Transactions
 
-  def call do
-    user = MarkevichMoney.Users.get_user!(1)
+  def call(user_id) do
+    user = MarkevichMoney.Users.get_user!(user_id)
+
+    payload = %{
+      current_user: user
+    }
+
+    payload
+    |> calculate_previous_month_stats()
+    |> calculate_two_month_ago_stats()
+    |> union_previous_month()
+    |> union_two_month_ago()
+    |> inject_diff()
+    |> group_by_folders()
+    |> inject_folders_sum_and_diffs()
+    |> sort_folders()
+    |> sort_categories()
+    |> generate_table_data()
+    |> generate_table_output()
+    |> generate_output_message()
+    |> SendMessage.call()
+  end
+
+  defp calculate_previous_month_stats(%{current_user: user} = payload) do
     previous_month = Timex.shift(Timex.now(), months: -1)
 
     stats_previous_month =
@@ -13,6 +35,11 @@ defmodule MarkevichMoney.Pipelines.Reports.ComparedExpenses do
         Timex.end_of_month(previous_month)
       )
 
+    payload
+    |> Map.put(:stats_previous_month, stats_previous_month)
+  end
+
+  defp calculate_two_month_ago_stats(%{current_user: user} = payload) do
     two_month_ago = Timex.shift(Timex.now(), months: -2)
 
     stats_two_month_ago =
@@ -22,18 +49,25 @@ defmodule MarkevichMoney.Pipelines.Reports.ComparedExpenses do
         Timex.end_of_month(two_month_ago)
       )
 
-    previous_month =
-      stats_previous_month
-      |> Enum.map(fn row ->
+    payload
+    |> Map.put(:stats_two_month_ago, stats_two_month_ago)
+  end
+
+  defp union_previous_month(%{stats_previous_month: stats} = payload) do
+    union =
+      Enum.map(stats, fn row ->
         row
         |> Map.put(:sum_previous_month, Decimal.abs(row.sum))
         |> Map.put(:sum_two_month_ago, Decimal.new(0))
         |> Map.delete(:sum)
       end)
 
+    Map.put(payload, :union, union)
+  end
+
+  defp union_two_month_ago(%{union: union, stats_two_month_ago: stats} = payload) do
     union =
-      stats_two_month_ago
-      |> Enum.reduce(previous_month, fn row, acc ->
+      Enum.reduce(stats, union, fn row, acc ->
         index =
           Enum.find_index(acc, fn elem ->
             elem.category_id == row.category_id
@@ -57,21 +91,36 @@ defmodule MarkevichMoney.Pipelines.Reports.ComparedExpenses do
         end
       end)
 
-    union =
-      union
-      |> Enum.map(fn row ->
+    Map.put(payload, :union, union)
+  end
+
+  defp inject_diff(%{union: union} = payload) do
+    union_with_diff =
+      Enum.map(union, fn row ->
         diff = Decimal.sub(row.sum_previous_month, row.sum_two_month_ago)
 
         row
         |> Map.put(:diff, diff)
       end)
-      |> Enum.group_by(fn row ->
+
+    Map.put(payload, :union, union_with_diff)
+  end
+
+  defp group_by_folders(%{union: union} = payload) do
+    grouped_union =
+      Enum.group_by(union, fn row ->
         %{
           folder_name: row.folder_name,
           folder_with_single_category: row.folder_with_single_category
         }
       end)
-      |> Enum.map(fn {folder, categories} ->
+
+    Map.put(payload, :union, grouped_union)
+  end
+
+  defp inject_folders_sum_and_diffs(%{union: union} = payload) do
+    updated_union =
+      Enum.map(union, fn {folder, categories} ->
         sum_two_month_ago =
           Enum.reduce(categories, Decimal.new(0), fn row, sum ->
             Decimal.add(sum, row.sum_two_month_ago)
@@ -95,12 +144,37 @@ defmodule MarkevichMoney.Pipelines.Reports.ComparedExpenses do
           categories
         }
       end)
-      |> Enum.sort_by(fn {folder, _categories} -> Decimal.to_float(folder.diff) end, :desc)
 
-    output =
+    Map.put(payload, :union, updated_union)
+  end
+
+  defp sort_folders(%{union: union} = payload) do
+    updated_union =
+      Enum.sort_by(union, fn {folder, _categories} -> Decimal.to_float(folder.diff) end, :desc)
+
+    Map.put(payload, :union, updated_union)
+  end
+
+  defp sort_categories(%{union: union} = payload) do
+    updated_union =
+      Enum.map(union, fn {folder, categories} ->
+        sorted_categories =
+          Enum.sort_by(categories, fn category -> Decimal.to_float(category.diff) end, :desc)
+
+        {
+          folder,
+          sorted_categories
+        }
+      end)
+
+    Map.put(payload, :union, updated_union)
+  end
+
+  defp generate_table_data(%{union: union} = payload) do
+    table_data =
       Enum.reduce(union, [], fn {folder, categories}, acc ->
         if folder.folder_with_single_category do
-          acc ++
+          category_table_row =
             Enum.flat_map(categories, fn row ->
               diff = symbolic_diff(row.diff)
 
@@ -114,11 +188,13 @@ defmodule MarkevichMoney.Pipelines.Reports.ComparedExpenses do
                 ["", "", "", ""]
               ]
             end)
+
+          acc ++ category_table_row
         else
           diff = symbolic_diff(folder.diff)
 
-          acc =
-            acc ++
+          folder_row =
+            if Enum.count(categories) > 1 do
               [
                 [
                   folder.folder_name,
@@ -127,15 +203,24 @@ defmodule MarkevichMoney.Pipelines.Reports.ComparedExpenses do
                   diff
                 ]
               ]
+            else
+              [
+                [
+                  folder.folder_name,
+                  "",
+                  "",
+                  ""
+                ]
+              ]
+            end
 
-          sorted =
-            Enum.sort_by(categories, fn category -> Decimal.to_float(category.diff) end, :desc)
+          acc = acc ++ folder_row
 
-          rendered =
-            Enum.flat_map(sorted, fn category ->
+          category_row =
+            Enum.flat_map(categories, fn category ->
               diff = symbolic_diff(category.diff)
 
-              if List.last(sorted) == category do
+              if List.last(categories) == category do
                 [
                   [
                     "└#{category.category_name}",
@@ -157,27 +242,33 @@ defmodule MarkevichMoney.Pipelines.Reports.ComparedExpenses do
               end
             end)
 
-          acc ++ rendered
+          acc ++ category_row
         end
       end)
 
-    table =
-      output
+    Map.put(payload, :table_data, table_data)
+  end
+
+  defp generate_table_output(%{table_data: table_data} = payload) do
+    table_as_string =
+      table_data
       |> TableRex.Table.new(["Категория", "06.2021", "07.2021", "Разница"], "Сравнение расходов")
       |> TableRex.Table.put_column_meta(:all, align: :left, padding: 0)
       |> TableRex.Table.render!(horizontal_style: :off, vertical_style: :off)
 
-    %{
-      current_user: user,
-      output_message: """
-      ```
+    Map.put(payload, :table_output, table_as_string)
+  end
 
-      #{table}
+  defp generate_output_message(%{table_output: table_output} = payload) do
+    message = """
+    ```
 
-      ```
-      """
-    }
-    |> SendMessage.call()
+    #{table_output}
+
+    ```
+    """
+
+    Map.put(payload, :output_message, message)
   end
 
   defp symbolic_diff(value) do
