5bc28dde1842a36718e6df9ac6d4c0f10852818c
markevich <slava.markevich@gmail.com>
2021-09-28 00:59:57 +0400

    Update libs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import "../css/app.css"
 
 import "phoenix_html"
 import { Socket } from "phoenix"
