de00b6701e9633eb9f0232118424808dbc46f9ed
Slava Markevich <slava.markevich@gmail.com>
2022-02-24 17:33:46 +0400

    Switch to github actions (#476)

--- config/test.secret.example.exs
+++ config/test.secret.example.exs
@@ -12,5 +12,11 @@ config :markevich_money, MarkevichMoney.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+if System.get_env("GITHUB_ACTIONS") do
+  config :markevich_money, MarkevichMoney.Repo,
+    url: System.get_env("DATABASE_URL"),
+    show_sensitive_data_on_connection_error: true
+end
+
 config :nadia, token: "bot token"
 config :markevich_money, priorbank_api_url: "https://www.fobar.foo"
