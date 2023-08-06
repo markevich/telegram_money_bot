37b10113dd3af722f29db22add12f433298f5d4f
markevich <slava.markevich@gmail.com>
2023-08-06 15:50:19 +0400

    Fix big numbers parsing on alfa bank messages

--- lib/markevich_money/steps/transaction/parse_amount_and_currency.ex
+++ lib/markevich_money/steps/transaction/parse_amount_and_currency.ex
@@ -1,6 +1,6 @@
 defmodule MarkevichMoney.Steps.Transaction.ParseAmountAndCurrency do
   # credo:disable-for-next-line
-  @regex ~r/Сумма:((?<amount>\d+\.?\d*)\s(?<currency>\w{3}))(\s\((?<external_amount>\d+\.?\d*)\s(?<external_currency>\w{3})\))?/
+  @regex ~r/Сумма:((?<amount>[\d\s]+\.?\d*)\s(?<currency>\w{3}))(\s\((?<external_amount>[\d\s+]+\.?\d*)\s(?<external_currency>\w{3})\))?/
 
   def call(%{input_message: input_message} = payload) do
     payload
@@ -21,15 +21,22 @@ defmodule MarkevichMoney.Steps.Transaction.ParseAmountAndCurrency do
          "external_currency" => external_currency
        }) do
     if external_amount == "" do
-      {origin_card_amount, _} = Float.parse(amount)
+      {origin_card_amount, _} =
+        Regex.replace(~r/\s/u, amount, "")
+        |> Float.parse()
 
       %{amount: origin_card_amount, currency_code: currency}
     else
       # If we have two currencies in transaction, ie "11 USD (25 BYN)",
       # then first number is `external_amount`, second is `amount`
 
-      {origin_card_amount, _} = Float.parse(external_amount)
-      {external_card_amount, _} = Float.parse(amount)
+      {origin_card_amount, _} =
+        Regex.replace(~r/\s/u, external_amount, "")
+        |> Float.parse()
+
+      {external_card_amount, _} =
+        Regex.replace(~r/\s/u, amount, "")
+        |> Float.parse()
 
       %{
         amount: origin_card_amount,
